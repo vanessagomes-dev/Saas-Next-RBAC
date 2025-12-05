67f8d723c21edaec154ef152041e501c99e68c20
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-12-04 23:37:50 -0300

    feat(organization): adiciona criação de nova organização com layout atualizado e formulário completo
    Implementada a tela de criação de organização, incluindo:
    
    Ajustes visuais e novos estilos para melhorar a experiência do usuário.
    
    Formulário com campos para nome da organização, domínio e checkbox para opção de convidar automaticamente membros com o mesmo domínio por e-mail.
    
    Correções de layout, estrutura e integração dos componentes garantindo o funcionamento adequado do fluxo de criação.

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,11 +1,5 @@
-import { Header } from '@/components/header'
 import React from 'react'
 
 export default async function Home() {
-  return (
-    <div className="py-4">
-      <Header />
-      <main></main>
-    </div>
-  )
+ return <h1>Projects</h1>
 }
--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -10,7 +10,7 @@ import { Separator } from './ui/separator'
 export async function Header() {
   const permissions = await ability()
   return (
-    <div className="mx-auto flex max-w-[1200px] items-center justify-between">
+    <div className="mx-auto flex max-w-[1200px] items-center justify-between border-b pb-2">
       <div className="flex items-center gap-3">
         {/* <Image
           src=""
